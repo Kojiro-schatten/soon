56f0537acfad0a1f9d427b22a0e2abb53f643354
KojiroSchatten <banbirou@icloud.com>
2021-09-19 10:10:04 +0900

    feat: navigation component

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+import { ImageURISource } from 'react-native';
+import { NavigatorScreenParams } from '@react-navigation/native';
+
+export type BoatParams = {
+  name: string
+  text: string
+  image: ImageURISource
+  credit: string
+}
+
+export type ModalNavigatorParamsList = {
+  BoatInformation: BoatParams
+  Definition: { word: string; definition: string }
+}
+
+export type TopNavigatorParamsList = {
+  Splash: undefined
+  Tabs: undefined
+}
+
+export type RootNavigatorParamsList = {
+  Main: NavigatorScreenParams<TopNavigatorParamsList>
+  Modal: NavigatorScreenParams<ModalNavigatorParamsList>
+}
+
+export type RootStackParamsList = {
+  Main: undefined
+  Auth: undefined
+  Modal: undefined
+}
